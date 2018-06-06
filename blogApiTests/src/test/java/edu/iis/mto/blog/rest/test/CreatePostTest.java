73a56ef7b8f0c30b04135e73896c9ce5d198d33c
Klaudia Goczal <klaudia@lodz.pl>
2018-06-06 11:50:32 +0200

    3.4.2 Functional test for adding post by confirmed user. Added throwing DomainError

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreatePostTest.java
@@ -0,0 +1,36 @@
+package edu.iis.mto.blog.rest.test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class CreatePostTest extends FunctionalTests {
+
+    public static final String CONFIRMED_USER = "/blog/user/1/post";
+    public static final String NEW_USER = "/blog/user/2/post";
+
+    @Test
+    public void addingPostByCONFIRMEDUserReturnsCreatedStatus() {
+        JSONObject jsonObj = new JSONObject().put("entry", "Test entry");
+
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when().post(CONFIRMED_USER);
+    }
+
+    @Test
+    public void addingPostByNewUserReturnBadRequest() {
+        JSONObject jsonObj = new JSONObject().put("entry", "Test");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when().post(NEW_USER);
+
+    }
+}
+
+
+
+
+
+//Wyszukanie postów użytkownika. Wyszukiwanie postów użytkownika o statusie usunięty nie powinno się powieść. Wyszukiwanie postów powinno zwracać poprawną liczbę polubień.
+//Wyszukanie użytkowników. Rezultat zwracany przez system nie powinien zawierać informacji o użytkownikach usuniętych.
